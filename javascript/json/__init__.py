510c5cd94db5e8d2d07e9e90c90d0213baf8815d
Farrell Raafi <farrell@odooabc.com>
2020-08-01 11:32:03 +0700

    Add some functions to build multi-hierarchy, multi-types json

--- javascript/json/__init__.py
+++ javascript/json/__init__.py
@@ -1,1 +1,29 @@
 from . import stringify
+
+def fromString(value):
+    if value is None: return 'null'
+    return '"%s"' % (value)
+
+def fromInt(value):
+    return '%s' % (value)
+
+def fromFloat(value):
+    return repr(value)
+
+def fromBoolean(value):
+    return 'true' if value == True else 'false' if value == False else 'null'
+
+def fromDict(value): #string only
+    if value is None: return 'null'
+    json = '{'
+    index = 0
+    for key in value:
+        if index > 0: json += ','
+        json += '"%s":%s' % (key, value[key])
+        index += 1
+    json += '}'
+    return json
+
+def fromList(value):
+    if value is None: return 'null'
+    return '[' + ','.join(value) + ']'
--- javascript/types.py
+++ javascript/types.py
@@ -6,19 +6,11 @@ NoneType = type(None)
 
 str = str
 
-def string(): return '""'
-
 int = int
-
-def integer(): return '%s' % (0)
 
 float = float
 
-def float(): return repl(0.0)
-
 bool = bool
-
-def boolean(): return 'false'
 
 dict = dict
 
